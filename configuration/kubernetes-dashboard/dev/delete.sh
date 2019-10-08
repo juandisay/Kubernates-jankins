49eae63196d32f4fc367aed7771f9a27e093acaa
juandisay <juandi.syafrin@gmail.com>
2019-10-08 18:21:44 +0700

    revisi

--- configuration/kubernetes-dashboard/dev/delete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Author: juandisay
-
-echo "install dashboard"
-kubectl delete -f namespace.yaml -f kubernetes-dashboard.yaml -f metric-scraper.yaml
-
-echo "set user admin"
-kubectl delete -f admin-token.yaml
--- configuration/kubernetes-dashboard/dev/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Author: juandisay
-
-echo "install dashboard"
-kubectl apply -f namespace.yaml -f kubernetes-dashboard.yaml -f metric-scraper.yaml
-
-echo "set user admin"
-kubectl apply -f admin-token.yaml
