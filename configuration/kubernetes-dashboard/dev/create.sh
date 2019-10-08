a238b8daeb88e363a5d46e8ef0b808d48df61ef8
juandisay <juandi.syafrin@gmail.com>
2019-10-08 18:15:36 +0700

    revisi

--- configuration/kubernetes-dashboard/dev/create.sh
+++ configuration/kubernetes-dashboard/dev/create.sh
@@ -2,7 +2,7 @@
 # Author: juandisay
 
 echo "install dashboard"
-kubectl create -f namespace.yaml -f kubernetes-dashboard.yaml -f metric-scraper.yaml
+kubectl create -f ./namespace.yaml -f ./kubernetes-dashboard.yaml -f ./metric-scraper.yaml
 
 echo "set user admin"
-kubectl create -f admin-token.yaml
+kubectl create -f ./admin-token.yaml
